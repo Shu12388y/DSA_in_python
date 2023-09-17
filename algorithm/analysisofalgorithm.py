dc0af5d23a54a64e920c9156840ab66ca78442b1
Shu12388y <spl.sp999@gmail.com>
2023-09-17 12:22:40 +0530

    file added

--- algorithm/analysisofalgorithm.py
+++ algorithm/analysisofalgorithm.py
@@ -1,14 +1,31 @@
 # write a function that take a input and print sum of n natural number
-def sum():
-    number=int(input("enter your number "))
+# time complexity is n
+def sum(n):
     temp=0
-    for x in range(number+1):
+    for x in range(1,n+1):
         temp=temp+x
     return temp    
+# time complexity is 1
+
+def fun1(n):
+    sum=(n*(n+1))/2
+    return sum
+
+
+# time complexity is n*n
+def fun3(n):
+    sum=0
+    for i in range(1,n+1):
+        for j in range(1,i+1):
+            sum=sum+1
+    return sum        
+
+
 
 
 def main():
-    print(sum())
+    n=int(input("Enter the number "))
+    print(fun3(n))
 
 
 
