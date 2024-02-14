d93090d15fb29217072f5968b6f3071163b52510
Shu12388y <spl.sp999@gmail.com>
2024-02-14 06:51:19 +0530

    file added

---
+++ list/bash.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+git add .
+git commit -m "file added"
+git push
+echo "done ....."
--- list/reverse.py
+++ list/reverse.py
@@ -1,5 +1,9 @@
-def reverseList(l):
-    reverseOrder = l.reverse()
-    return reverseOrder
+def reverse_list(l):
+    newlist = list()
+    for i in range(len(l)):
+        newlist.append(l[len(l)-i-1])
+    return newlist    
+# time complexity O(n)
 
-print(reverseList([1,2,3,4]))    
+
+print(reverse_list([11,2,3,4,5,0,87,4]))
