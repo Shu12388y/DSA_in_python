f692de8342a53d0c88a00b7b15ab7cff1f905b8f
Shu12388y <spl.sp999@gmail.com>
2024-04-10 21:37:04 +0530

    file added

---
+++ hashing/orange.py
@@ -0,0 +1,17 @@
+def orangeandapple(apples,orange,s,t,a,b):
+    apples_count = 1
+    orange_count =1
+    for i in range(len(apples)):
+        apples[i] = apples[i]+a
+        if apples[i] >=s and apples[i]<=t:
+            apples_count = apples_count +1 
+    for i in range(len(orange)):
+        orange[i] = orange[i]+b
+        if orange[i] >=s and orange[i] <= t:
+            orange_count = orange_count +1       
+    print(apples_count)
+    print(orange_count)
+
+
+
+orangeandapple([2,3,-4],[3,-2,-4],7,10,4,12)
