3e2e300316d5e7d4830606c32fa7b92fbe57e088
Shu12388y <spl.sp999@gmail.com>
2024-04-11 20:13:13 +0530

    file addeD

---
+++ patterns/pattern1.py
@@ -0,0 +1,18 @@
+def pattern1():
+    for i in range(5):
+        for j in range(6):
+            print("*",end="")
+        print()
+
+
+
+
+
+pattern1()
+
+# output:
+# ******
+# ******
+# ******
+# ******
+# ******
