8c9fd1c6a4d5e1d38816444e757e9f9a59943554
Shu12388y <spl.sp999@gmail.com>
2024-04-11 20:37:12 +0530

    file addeD

---
+++ patterns/pattern2.py
@@ -0,0 +1,46 @@
+def pattern2():
+     for i in range(6):
+        for j in range(1,i):
+            print(i%2,end="")
+        print()    
+
+
+
+pattern2()    
+
+
+# output 1:
+# *
+# **
+# ***
+# ****
+
+# output 2:
+# 1
+# 22
+# 333
+# 4444
+
+# output 3:
+# 0
+# 01
+# 012
+# 0123
+
+# output 4:
+# 1
+# 12
+# 123
+# 1234
+
+# output 5:
+# 1   
+# 10  
+# 101 
+# 1010
+
+# output 6:
+# 0   
+# 11  
+# 000 
+# 1111
---
+++ patterns/pattern3.py
@@ -0,0 +1,9 @@
+def pattern3():
+    for i in range(5):
+        for j in range(i,-1,-1):
+            print("*",end="")
+        print()    
+
+
+
+pattern3()
