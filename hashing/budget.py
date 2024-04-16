4eb076b133aa3036f0bf2cf2d06610b64d5497de
Shu12388y <spl.sp999@gmail.com>
2024-04-16 07:53:33 +0530

    file added

--- hashing/budget.py
+++ hashing/budget.py
@@ -1,22 +1,22 @@
 def budget(k,m,b):
-            price = []
-            for i in range(len(k)):
-                temp = k[i]
-                for j in range(len(m)):
-                    if(temp+m[j] <= b):
-                        price.append(temp + m[j])                                                   
-            max_num = max(price)
-            if(max_num < b):
-                return max_num
-            else:
-                return -1                
-
-            # l=-1
+            # price = []
             # for i in range(len(k)):
+            #     temp = k[i]
             #     for j in range(len(m)):
-            #         if k[i]+m[j]>l  and k[i]+m[j]<b:
-            #             l = k[i]+m[j]
-            # return l                        
+            #         if(temp+m[j] <= b):
+            #             price.append(temp + m[j])                                                   
+            # max_num = max(price)
+            # if(max_num < b):
+            #     return max_num
+            # else:
+            #     return -1                
+
+            l=-1
+            for i in range(len(k)):
+                for j in range(len(m)):
+                    if k[i]+m[j]>l  and k[i]+m[j]<b:
+                        l = k[i]+m[j]
+            return l                        
 
                                   
 
