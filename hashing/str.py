5dea9feba5e1ccc41955f3091764ba2e7132773a
Shubham Paul <spl.sp999@gmail.com>
2024-05-03 14:09:18 +0530

    file added

--- hashing/str.py
+++ hashing/str.py
@@ -1,23 +1,10 @@
-def stringCount(st):
-    stringArray=[]
-    count = 0
-    first=[]
-    for i in st:
-        stringArray.append(i)
-    for i in range(len(stringArray)):
-        for j in range(i+1,len(stringArray)-1):
-            if(stringArray[i] != stringArray[j]):
-                first.append(stringArray[i])
-        
-        # if(i == i+1):
-        #     stringArray.remove(i)
-        #     stringArray.remove(i+1)
-
-  
-          
-
-    return first
+def firstUniqChar(self, s: str) -> int:
+        hash  = dict()
+        for char in s:
+            hash[char] = hash.get(char,0)+1
+        for i,char in enumerate(s):
+            if hash[char] == 1:
+                return i
 
 
-
-print(stringCount("leetcode"))
+        return -1 
