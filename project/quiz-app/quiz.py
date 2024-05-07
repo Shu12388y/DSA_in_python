820b8d26779d1bdf9dc4804a161655039d9bc5df
Shu12388y <spl.sp999@gmail.com>
2024-05-07 19:49:34 +0530

    file added

---
+++ project/quiz-app/quiz.py
@@ -0,0 +1,18 @@
+question = [["1. What is the largest planet of the solar system","a. Sun","b. Earth"],["2. what is full form of IP","a. Internet Protocol","b.  Inter Protocol"]]
+answers = ["a","b"]
+
+
+def quizRender(arr):
+    score = 0
+    for i in range(len(arr)):
+        print(str(arr[i]),end="")
+        ans = input("Enter you answer: ")
+        if(ans==answers[i]):
+            score += 1
+    print(score)
+
+quizRender(question)            
+
+
+
+
