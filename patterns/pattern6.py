cd260d6cfec04e1e70687a27e65453a7ea444683
Shu12388y <spl.sp999@gmail.com>
2024-06-03 12:19:18 +0530

    file added

--- patterns/pattern6.py
+++ patterns/pattern6.py
@@ -31,13 +31,36 @@ def pattern5():
     for i in range(0,6):
         for j in range(1,6-i):
             print(j,end="")
-        print()    
+        print()  
+
+
+
+def pattern6():
+    for i in range(0,5):
+        for j in range(0,5-i+1):
+            print(" ",end="")
+        for j in range(0,2*i+1):
+            print("*",end="")
+        for j in range(0,5-i+1):
+            print(" ",end="")        
+        print()            
+
+
+
+
+
+
+
+
+
+
 
 def main():
     # pattern1()
     # pattern2()
     # pattern3()
     # pattern4()
-    pattern5()
+    # pattern5()
+    pattern6()
 
 main()
