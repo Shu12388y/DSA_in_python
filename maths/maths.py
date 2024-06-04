2887fb596d5c5cbbaf07da9f88fb1e48d0a59cec
Shu12388y <spl.sp999@gmail.com>
2024-06-04 14:19:42 +0530

    q2 added

--- maths/maths.py
+++ maths/maths.py
@@ -12,22 +12,44 @@ def functionMaths1(n):
     
 
 # count the number of digits
-
+# T.C. O(ln10(n))
 def countDigit(n):
     count = 0
     while n>1:
-        last  = n%10
-        if last:
-            count +=1
+        count +=1
         n = n/10
     return count         
 
-# solution 2
+# solution 2 (oprtimis)
 
 def countDigits(n):
     cnt = int(math.log10(n) + 1)
-
     return cnt
+
+
+
+
+#  Reverse the number 
+def reverseNums(n):
+    reversen = 0
+    last = 0 
+    while n>1:
+        last = n%10
+        n = n/10
+        reversen = (reversen*10)+int(last)
+    return reversen    
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -35,7 +57,8 @@ def countDigits(n):
 def main():
     # functionMaths1(1234)
     # print(countDigit(12345))
-    print(countDigits(12345))
+    # print(countDigits(12345))
+    print(reverseNums(12345))
 
 
 
