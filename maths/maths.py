8361881e4d14f6ee0706e7bf75017c4ec273746a
Shu12388y <spl.sp999@gmail.com>
2024-06-04 13:30:39 +0530

    FILE ADDED

---
+++ maths/maths.py
@@ -0,0 +1,27 @@
+# maths in coding
+
+
+
+def functionMaths1(n):
+    # get the digit of number
+    while n>=1:
+        last=n%10
+        print(int(last))  
+        n = n//10
+
+    
+
+
+
+
+
+
+
+
+
+def main():
+    functionMaths1(1234)
+
+
+
+main()    
