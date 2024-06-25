db470cbd62554714f00e5acecfe17be456ae8826
Shu12388y <spl.sp999@gmail.com>
2024-06-25 11:32:26 +0530

    added the file

--- sorting/inserionSort.py
+++ sorting/inserionSort.py
@@ -14,12 +14,12 @@
 
 def insertionSort(n: list):
     for i in range(1, len(n)):
-        key = n[i]
-        j = i - 1
-        while j >= 0 and key < n[j]:
-            n[j + 1] = n[j]
-            j -= 1
-        n[j + 1] = key
+        key = n[i]                              # key = 5  [5,20,40,60,10,30]        40                             60                  10      
+        j = i - 1                               # j = 0                               1                             2                   3
+        while j >= 0 and key < n[j]:            # j> =  and 5 < 20 True            j>=0 and 40 < 20  False    j>=0 and 60<40 False     j>=0 and 10<60 True
+            n[j + 1] = n[j]                       # n[0+1] = n[0]   n[1] = 20                                                           n[3+1] = n[3]   n[4] = 60 
+            j -= 1                                     # j = -1                                                                             j -= 1   n[2+1] = n[2]   n[3] = 40  j-=1  n[1+1] = n[1]  n[2] = 20   j-=1  n[0+1] = n[0]   n[1] = 5  false
+        n[j + 1] = key                              # n[-1+1] = n[0] = 5                                                                        n[-1 + 1]  = 10
     return n
 
 print(insertionSort([20,5,40,60,10,30]))        
--- sorting/merge.py
+++ sorting/merge.py
@@ -10,17 +10,52 @@ def mergeSort(m:list,n:list):
 
 def mergeList(m:list,n:list)->list:
     res = []
-    for i in range(len(m)-1):
-        for j in range(len(n)):
-            if m[i] > n[j]:
-                res.append(m[i])
-                i += 1
-            else:
-                res.append(n[j])
-                j += 1
-    return res        
-    
-print(mergeList([1,5,4,3],[6,9,8]))
+    i = 0
+    j = 0
+    while i<len(m) and j<len(n):
+        if m[i] < n[j]:
+            res.append(m[i])
+            i = i+1
+        else:
+            res.append(n[j])
+            j = j+1     
+    while i<len(m):
+        res.append(m[i])
+        i = i+1
+    while j < len(n):
+        res.append(n[j])
+        j = j+1
+    return res         
+
+# print(mergeList([1,5,4,3],[6,9,8]))   # unsorted list
+
+# question:  write a function that take an array as input and you have low, mid and high so you have to sort the list
+#
 
 
+def sortingList(a: list, low: int, mid: int, high: int):
+    left = a[low:mid]
+    right = a[mid+1:high]
+    i = 0
+    j = 0 
+    k = low
+    while i<len(left) and j<len(right):
+        if left[i] <= right[j]:
+            a[k] = left[i]
+            k = k+1
+            i = i+1
+        else:
+            a[k] = right[j]
+            k += 1
+            j += 1
+    while i < len(left):
+        a[k] = left[i]
+        i += 1
+        k += 1
+    while j < len(right):
+        a[k] = right[j]
+        j += 1
+        k += 1
+    return a                          
 
+print(sortingList([10,15,20,11,13],0,2,4))
