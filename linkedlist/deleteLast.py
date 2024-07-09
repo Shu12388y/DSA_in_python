aaef1b295ef1fed7ec9786b7774d46ede90014ff
Shu12388y <spl.sp999@gmail.com>
2024-07-09 12:20:11 +0530

    code added

---
+++ linkedlist/deleteLast.py
@@ -0,0 +1,24 @@
+class Node:
+    def __init__(self,key) -> None:
+        self.key = key
+        self.next = None
+
+
+
+def deleteLast(h:object):
+    if h == None:
+        return None
+    if h.next == None:
+        return None
+    curr  =  h 
+    while curr.next.next != None:
+        curr = curr.next
+    curr.next = None
+    return h 
+
+
+head = Node(10)
+head.next = Node(16)
+
+
+deleteLast(head)
