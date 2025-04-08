5fd400ea2d9f6861569d9dd667e1196f5acd7fae
shub12388y <spl.sp999@gmail.com>
2025-04-08 12:38:08 +0530

    added the new form

--- binarysearch/perfectsqr.py
+++ binarysearch/perfectsqr.py
@@ -7,3 +7,10 @@ def findPerfectSqrt(x:int):
         return True
     else:
         return False
+    
+
+def findPerfect(x:int):
+    x **= 0.5
+    num_pref = x//1
+    return x == num_pref
+    
